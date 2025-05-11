c29254b5d77e4dbac259cc137305042a7d9d4fd8
Matt Connors <foxitb123@gmail.com>
2025-05-11 12:15:44 +0300

    Created solution with Hashmap and added timers
    - Finished HashMapTwoSum()
    - Added timers with Datetime to measure time elapsed between starting the operation and finishing

--- LeetCode.TwoSum/On.cs
+++ LeetCode.TwoSum/On.cs
@@ -1,10 +1,24 @@
-﻿namespace LeetCode.TwoSum
+﻿using System.Collections;
+
+namespace LeetCode.TwoSum
 {
     internal class On // Solutions with a time complexity of O(n)
     {
         public static int[] HashMapTwoSum(int[] nums, int target)
         {
-
+            Dictionary<int, int> HashMap = new Dictionary<int, int>(); // Initialize dictionary
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i]; // Complement == what sums up with nums[i] to target
+                if (HashMap.ContainsKey(complement)) // If complement is a key in the dictionary
+                {
+                    return new int[] { HashMap[complement], i }; // Return the indices of the two numbers
+                } else
+                {
+                    HashMap[nums[i]] = i; // Add the current number and its index to the dictionary
+                } // Iteration over, start again with the next number (if solution not already found)
+            }
+            throw new Exception($"Could not find a TwoSum solution given the array {nums} and the target {target}");
         }
     }
 }
--- LeetCode.TwoSum/Program.cs
+++ LeetCode.TwoSum/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode.TwoSum
+﻿using System;
+
+namespace LeetCode.TwoSum
 {
     internal class Program
     {
@@ -6,9 +8,20 @@
         {
             int[] nums = { 2, 7, 11, 15 };
             int target = 9;
-            
-            int[] result = OnSquared.SimpleTwoSum(nums, target);
-            Console.WriteLine($"Indices of the two numbers that add up to {target} are: {{{result[0]}, {result[1]}}}");
+
+            DateTime start1 = DateTime.Now;
+            int[] result1 = OnSquared.SimpleTwoSum(nums, target);
+            Console.WriteLine($"[O(n^2)] Indices of the two numbers that add up to {target} are: {{{result1[0]}, {result1[1]}}}");
+            DateTime end1 = DateTime.Now;
+            TimeSpan elapsed1 = end1 - start1;
+            Console.WriteLine($"Elapsed time: {elapsed1.TotalMilliseconds} ms");
+
+            DateTime start2 = DateTime.Now;
+            int[] result2 = On.HashMapTwoSum(nums, target);
+            Console.WriteLine($"[O(n)] Indices of the two numbers that add up to {target} are: {{{result2[0]}, {result2[1]}}}");
+            DateTime end2 = DateTime.Now;
+            TimeSpan elapsed2 = end2 - start2;
+            Console.WriteLine($"Elapsed time: {elapsed2.TotalMilliseconds} ms");
         }
     }
 }
